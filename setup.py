47a8f6de939b97213bd0cea2261c52bf686436a1
valeriacaro <124155861+valeriacaro@users.noreply.github.com>
2023-09-30 10:13:43 +0200

    Feat: Create project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DL project useful to predict if a product reaches the top standards depending on reviews made by clients on e-commerce',
+    author='sexism_identifiers (Valèria Caro, Esther Fanyanàs and Claudia Len)',
+    license='MIT',
+)
