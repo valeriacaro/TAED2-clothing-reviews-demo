23efa9ddb6cb7650c8449d451aef72fa2051e42b
valeriacaro <124155861+valeriacaro@users.noreply.github.com>
2023-10-01 17:56:52 +0200

    Revert "Feat preprocessing"

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,3 +1,4 @@
+import pandas
 import pandas as pd
 import altair as alt
 import nltk
@@ -5,25 +6,6 @@ from nltk.corpus import stopwords
 import string
 from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
-import kaggle
-
-def download_df():
-    """
-    Downloads kaggle dataset and saves it in the repository.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    download_dir = "/Users/claudialen/Dropbox/UNI/7eQuadri/TAED2/clothing-reviews/data/external"
-    username = "nicapotato"
-    dataset_name = "womens-ecommerce-clothing-reviews"
-
-    # Download the dataset in CSV format
-    kaggle.api.dataset_download_files(f"{username}/{dataset_name}", path=download_dir, unzip=True)
-
 
 
 def create_df() -> pd.DataFrame:
@@ -36,8 +18,9 @@ def create_df() -> pd.DataFrame:
         Returns:
             DataFrame: The DataFrame with the csv file's data.
     """
-    dataframe = pd.read_csv("/Users/claudialen/Dropbox/UNI/7eQuadri/TAED2/clothing-reviews/data/external/Womens Clothing E-Commerce Reviews.csv")
+    dataframe = pd.read_csv(...)
     return dataframe
+
 
 def dropping(dataframe) -> pd.DataFrame:
     """
@@ -50,7 +33,7 @@ def dropping(dataframe) -> pd.DataFrame:
             dataframe: The changed DataFrame.
     """
     dataframe.drop(
-        ["Title", "Positive Feedback Count", "Division Name", "Department Name", "Class Name", "Age",
+        ["Unnamed:0", "Title", "Positive Feedback Count", "Division Name", "Department Name", "Class Name", "Age",
          "Clothing ID", "Rating"], axis=1, inplace=True)
     dataframe.drop_duplicates(inplace=True)
     return dataframe
@@ -83,7 +66,6 @@ def tokenization(dataframe) -> pd.DataFrame:
     """
     nltk.download("punkt")
     nltk.download("stopwords")
-
     english_sw = set(stopwords.words('english') + list(string.punctuation))
     dataframe['Review Text'] = dataframe['Review Text'].apply(
         lambda x: " ".join([w.lower() for w in word_tokenize(str(x)) if w.lower() not in english_sw]))
@@ -105,27 +87,10 @@ def stemmed_text(dataframe) -> pd.DataFrame:
     dataframe['Stemmed Review Text'] = dataframe['Review Text'].apply(lambda text: " ".join([stem.stem(w) for w in text]))
     return dataframe
 
-def new_dataset(dataframe):
-    """
-    Saves processed dataset in csv file.
-
-    Args:
-        dataframe (DataFrame): Input DataFrame to be saved.
-
-    Returns:
-        None.
-    """
-    csv_file_path = "/Users/claudialen/Dropbox/UNI/7eQuadri/TAED2/clothing-reviews/data/interim/preprocessed_data.csv"
-    dataframe.to_csv(csv_file_path, index=False)
-
-
 if __name__ == '__main__':
-    download_df()
     df = create_df()
     df = binarization(df)
     df = dropping(df)
     df = tokenization(df)
     df = stemmed_text(df)
-    new_dataset(df)
-
 
