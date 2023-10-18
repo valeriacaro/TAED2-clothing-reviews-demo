d79a00794fca58fbe0f03c32abd95640350baac4
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-18 19:49:33 +0200

    fix_paths fix_dvc_files

--- src/features/prepare.py
+++ src/features/prepare.py
@@ -1,34 +1,26 @@
-from pathlib import Path
 import yaml
 from src.data.process_data import *
 from src.data.preprocess_data import *
+from src import *
 
-params_path = Path("../../params.yaml")
+if __name__ == '__main__':
 
-get_data_from_source()
+    params_path = Path("../../params.yaml")
 
-path_to_raw_data = "../../data/raw/raw_data.csv"
-data = get_data_from_local(path_to_raw_data)
+    data = load_data_from_remote(RAW_DATA_PATH)
 
-with open(params_path, "r") as params_file:
-    try:
-        params = yaml.safe_load(params_file)
-        params = params["prepare"]
-    except yaml.YAMLError as exc:
-        print(exc)
+    with open(params_path, "r") as params_file:
+        try:
+            params = yaml.safe_load(params_file)
+            params = params["prepare"]
+        except yaml.YAMLError as exc:
+            print(exc)
 
-data = clean_df(data)
-data = process_df(data)
+    data = clean_df(data)
+    data = process_df(data)
 
-train_data = data.sample(frac=params["train_size"], random_state=params["random_state"])
-test_data = data.drop(train_data.index)
+    train_data = data.sample(frac=params["train_size"], random_state=params["random_state"])
+    test_data = data.drop(train_data.index)
 
-
-path_to_processed = "data/processed"
-
-train_data_path = path_to_processed + "/processed/train_data_processed.csv"
-test_data_path = path_to_processed + "/test_data_processed.csv"
-
-
-save_data_to_local(train_data_path, train_data)
-save_data_to_local(test_data_path, test_data)
+    save_data_to_local(PROCESSED_DATA_PATH, train_data)
+    save_data_to_local(PROCESSED_DATA_PATH, test_data)
