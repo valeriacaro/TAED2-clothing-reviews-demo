50aa77d52b352df0d32d07a436ecea16699cc3d8
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-22 19:32:03 +0200

    feat_pytest_preprocess_data

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -49,6 +49,7 @@ def binarization(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
 
     dataframe.loc[dataframe['Rating'] <= 4, 'Recommended IND'] = 0
+    dataframe.loc[dataframe['Rating'] > 4, 'Recommended IND'] = 1
     dataframe.rename(
         columns={'Recommended IND': 'Top Product'}, inplace=True
     )
--- src/tests/test_preprocess_data.py
+++ src/tests/test_preprocess_data.py
@@ -3,16 +3,23 @@ This module is created to test functions stored
 on src.data.preprocess_data
 """
 import pandas as pd
+import pytest
 from src.data.preprocess_data import dropping, binarization, clean_df
 
 def test_dropping_output():
+    """
+    Checks if given a random dataframe
+    with some columns to delete by dropping,
+    but not with all on the list of the function,
+    just keeps important columns.
+    """
 
     data = {
         'Index': 1,
         'Review': 'good shirt',
-        'Age': '44',
-        'Rating': '5',
-        'Top Product': '1'
+        'Age': 44,
+        'Rating': 5,
+        'Top Product': 1
     }
 
     dataframe = pd.DataFrame(
@@ -26,9 +33,42 @@ def test_dropping_output():
     assert 'Rating' not in dataframe.columns
 
 
-def test_binarization_output():
+def test_binarization_output(Rate, Top):
+    """
+    Checks if given a dataframe with
+    a Rating column, binarization returns
+    the same dataframe without that column,
+    but with Top Product with the respective
+    mapping.
+    """
 
-    pass
+    data = {
+        'Index': 1,
+        'Rating': Rate
+    }
+    dataframe = pd.DataFrame(
+        data, index=['Index'], columns=['Rating']
+    )
+
+    dataframe = binarization(dataframe)
+
+    assert 'Top Product' in dataframe.columns
+    assert (dataframe['Top Product'] == Top).all()
+
+
+@pytest.mark.parametrize(
+    "Rating, Top_Product",
+    [
+        (5, 1),
+        (3, 0),
+        (4, 0),
+        (2, 0),
+        (1, 0)
+    ]
+)
+def test_binarization_output_parametrize(Rating, Top_Product):
+    test_binarization_output(Rating, Top_Product)
+
 
 def test_clean_df_output():
 
