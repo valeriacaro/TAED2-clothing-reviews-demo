fbb0366b7bca5374ac2730d21e721be4d40bced9
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-22 11:37:25 +0200

    fix_pylint_src/models/predict_model feat_.pylintrc

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,13 +1,23 @@
-from train_model import *
-import evaluate
+'''
+This module is created to store the function that
+gives predictions as output to decide if a
+product is a top product or not
+depending on the review given as input
+'''
 
+import evaluate
+import torch
+from torch import argmax
+import torch.nn.functional as F
 
 def prediction(eval_dataloader, model):
     """
 
     Args:
-        eval_dataloader (DataLoader): A DataLoader containing testing data.
-        model (transformers.BertForSequenceClassification): The machine learning model to be evaluated.
+        eval_dataloader (DataLoader):
+        - A DataLoader containing testing data.
+        model (transformers.BertForSequenceClassification):
+        - The machine learning model to be evaluated.
 
     Returns:
         None
@@ -39,15 +49,18 @@ def prediction(eval_dataloader, model):
         # Append the logits batch to the list
         logits_all.append(logits)
         # Get the predicted probabilities for the batch
-        predicted_prob = torch.softmax(logits, dim=1)
-        # Append the predicted probabilities for the batch to all the predicted probabilities
+        predicted_prob = F.softmax(logits, dim=1)
+        # Append the predicted probabilities for the batch to
+        # all the predicted probabilities
         predicted_prob_all.append(predicted_prob)
         # Get the predicted labels for the batch
-        predictions = torch.argmax(logits, dim=-1)
+        predictions = argmax(logits, dim=-1)
         # Append the predicted labels for the batch to all the predictions
         predictions_all.append(predictions)
         # Add the prediction batch to the evaluation metric
-        metric1.add_batch(predictions=predictions, references=batch["labels"])
+        metric1.add_batch(
+            predictions=predictions, references=batch["labels"]
+        )
 
     print(predictions_all)
 
