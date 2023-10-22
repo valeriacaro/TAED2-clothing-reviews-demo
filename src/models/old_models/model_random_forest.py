f74133a02c1fc172243c992cb16af30d793788b7
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-22 18:24:42 +0200

    feat_pylint_model_random_forest

--- src/models/old_models/model_random_forest.py
+++ src/models/old_models/model_random_forest.py
@@ -1,15 +1,19 @@
-# IMPORTS
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
+"""
+This module is created to contain all functions
+related to the random forest model.
+"""
+
 import joblib
 import mlflow
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
-from src.data.get_and_save_data import *
+from src.data.get_and_save_data import get_data_from_local
+from src import PROCESSED_TEST_DATA_PATH, PROCESSED_TRAIN_DATA_PATH
 
 
 def tracking():
@@ -19,23 +23,26 @@ def tracking():
      Returns:
          None
     """
-    mlflow.set_tracking_uri("https://github.com/MLOps-essi-upc/clothing-reviews/tree/develop/models")
+    mlflow.set_tracking_uri(
+        "https://github.com/MLOps-essi-upc/TAED2-clothing-reviews"
+        "/tree/develop/src/models/old_models"
+    )
     mlflow.autolog()
-
-
-def read_data():
-    """" Load and read the data"""
-    pass
 
 
 def stemming(df, stem=True) -> tuple:
     """
-     Preprocesses data and selects features and target based on the stemming flag.
+     Preprocesses data and selects features and target
+     based on the stemming flag.
      Args:
-         df (DataFrame): The input DataFrame.
-         stem (bool): A flag indicating whether stemming is applied.
+         df (DataFrame):
+         - The input DataFrame.
+         stem (bool):
+         - A flag indicating whether stemming is applied.
      Returns:
-         tuple: A tuple containing the feature variable (x) and the target variable (y).
+         tuple:
+         - A tuple containing the feature variable (x) and the
+         target variable (y).
     """
     if stem:
         x = df["Stemmed Review Text"]
@@ -52,7 +59,9 @@ def vectorization(x_train, x_test):
         x_train: Training text data.
         x_test: Testing text data.
     Returns:
-        tuple: A tuple containing TF-IDF vectors for training and testing data.
+        tuple:
+        - A tuple containing TF-IDF vectors for training
+        and testing data.
     """
 
     tf_idf_vectorizer = TfidfVectorizer()
@@ -61,7 +70,15 @@ def vectorization(x_train, x_test):
     return x_train_tf_idf, x_test_tf_idf
 
 
-def classification_task(model, x_train_scaled, y_train, x_test_scaled, y_test, predic, model_name):
+def classification_task(
+        model,
+        x_train_scaled,
+        y_train,
+        x_test_scaled,
+        y_test,
+        predic,
+        model_name
+):
     """
     Evaluates a classification model and returns performance metrics.
     Args:
@@ -71,9 +88,11 @@ def classification_task(model, x_train_scaled, y_train, x_test_scaled, y_test, p
         x_test_scaled: Scaled testing features.
         y_test: Testing labels.
         predic: Predicted labels.
-        model_name: Name of the model for identification in the results.
+        model_name: Name of the model for
+        identification in the results.
     Returns:
-        DataFrame: Performance metrics including accuracy, precision, recall, F1-score.
+        DataFrame: Performance metrics including
+        accuracy, precision, recall, F1-score.
     """
     perf_df = pd.DataFrame(
         {'Train_Score': model.score(x_train_scaled, y_train),
@@ -138,28 +157,28 @@ if __name__ == '__main__':
 
     tracking()
     # Load and preprocess the data
-    path_data = "./data/processed/processed_data.csv"
-    df = get_data_from_local(path_data)
+    test = get_data_from_local(PROCESSED_TEST_DATA_PATH)
+    train = get_data_from_local(PROCESSED_TRAIN_DATA_PATH)
 
     # Set this flag based on whether stemming is applied or not
-    use_stemming = True
-    x, y = stemming(df, use_stemming)
-
-    # Split the data into training and testing sets
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, stratify=y, random_state=101)
+    USE_STEMMING = True
+    x_train, y_train = stemming(test, USE_STEMMING)
+    x_test, y_test = stemming(test, USE_STEMMING)
 
     # Vectorize the text data
     x_train_tf_idf, x_test_tf_idf = vectorization(x_train, x_test)
 
     # Train and save the machine learning model
-    train_and_save_model(x_train_tf_idf, y_train, use_stemming)
+    train_and_save_model(x_train_tf_idf, y_train, USE_STEMMING)
 
     # Load the model
-    loaded_model = loading(use_stemming)
+    loaded_model = loading(USE_STEMMING)
     # Make predictions using the loaded model
     predictions = prediction(loaded_model, x_test_tf_idf)
 
     # Evaluate and print the model's performance
-    eval_fores = classification_task(loaded_model, x_train_tf_idf, y_train, x_test_tf_idf, y_test,
-                                     predictions, "Random Forest")
+    eval_fores = classification_task(
+        loaded_model, x_train_tf_idf, y_train, x_test_tf_idf, y_test,
+        predictions, "Random Forest"
+    )
     print(eval_fores)
